bdbc58531ce547ae579dca964038ee156f8f3b71
zeus012345 <francisjerry360@gmail.com>
2023-01-03 16:22:13 +0100

    added mobile screen

--- src/components/SharedLayout/Header/Header.tsx
+++ src/components/SharedLayout/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../../assets/logo.svg";
 import { Link } from "react-router-dom";
 import {
@@ -6,12 +6,20 @@ import {
   AiOutlineSearch,
   AiOutlineMenu,
   AiOutlineClose,
+  AiOutlineUser,
 } from "react-icons/ai";
-import { BsFillCaretDownFill } from "react-icons/bs";
+import { HiOutlineClipboardDocument } from "react-icons/hi2";
+import { BsFillCaretDownFill, BsFillCaretUpFill } from "react-icons/bs";
 import user from "../../../assets/user.png";
 import style from "./header.module.scss";
 
 const Header = ({ showNav, handleShowNav, setShowNav }: any) => {
+  const [showUser, setShowUser] = useState(false);
+
+  const handleShowUser = () => {
+    setShowUser(!showUser);
+  };
+
   return (
     <section>
       <div className={`flex ${style.user}`}>
@@ -47,6 +55,50 @@ const Header = ({ showNav, handleShowNav, setShowNav }: any) => {
           <div className={`flex ${style.user__name}`}>
             <span className="">Adedeji</span>
             <BsFillCaretDownFill />
+          </div>
+        </article>
+        {/* mobile user profile */}
+        <article className={style.user__mobile}>
+          <div className="flex">
+            <button>
+              <AiOutlineSearch />
+            </button>
+            <button onClick={handleShowUser} className="">
+              <AiOutlineUser />
+              <span className="">
+                {showUser ? <BsFillCaretUpFill /> : <BsFillCaretDownFill />}
+              </span>
+            </button>
+            {showUser ? (
+              <div
+                onClick={() => setShowUser(false)}
+                className={style.user__modal}
+              >
+                <Link to="#">
+                  <span className="">
+                    {" "}
+                    <img src={user} alt="" className="" />
+                  </span>
+                  <span className="">
+                    <span className="">Adedeji</span>
+                  </span>
+                </Link>
+                <Link to="#">
+                  <span className="">
+                    <HiOutlineClipboardDocument />
+                  </span>
+                  <span className="">Docs</span>
+                </Link>
+
+                <Link to="#">
+                  <span className="">
+                    <AiOutlineBell />
+                  </span>
+
+                  <span className="">Notification</span>
+                </Link>
+              </div>
+            ) : null}
           </div>
         </article>
       </div>
