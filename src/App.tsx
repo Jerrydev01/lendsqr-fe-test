0c2194649e9e17510d5af3e8c2abf55797865aa1
zeus012345 <francisjerry360@gmail.com>
2023-01-03 20:08:26 +0100

    added vercel

--- src/App.tsx
+++ src/App.tsx
@@ -9,38 +9,39 @@ import Guarantor from "./pages/guarantor";
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  
+
   // save login session to local storage
   const login = () => {
     localStorage.setItem("isAuthenticated", "true");
     setIsAuthenticated(true);
   };
 
-  // automatic redirect if auth is false
-  if (isAuthenticated === false) {
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login login={login} />} />
-          <Route path="*" element={<h1>404: Not found</h1>} />
-        </Routes>
-      </BrowserRouter>
-    );
-  } else {
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login login={login} />} />
-          <Route path="/" element={<SharedLayout />}>
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/guarantor" element={<Guarantor />} />
-            <Route path="/users" element={<Users />} />
-            <Route path="/dashboard/:userId" element={<DashboardUsers />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    );
-  }
+  // save logout session to local storage
+  const logout = () => {
+    localStorage.setItem("isAuthenticated", "false");
+    setIsAuthenticated(false);
+  };
+
+  // check if user is authenticated
+  // if (localStorage.getItem("isAuthenticated") === "true" && !isAuthenticated) {
+  //   setIsAuthenticated(true);
+  // } else {
+  //   setIsAuthenticated(false);
+  // }
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login login={login} />} />
+        <Route path="/" element={<SharedLayout />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/guarantor" element={<Guarantor />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/dashboard/:userId" element={<DashboardUsers />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
