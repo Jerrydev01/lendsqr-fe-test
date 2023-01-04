1b293cfa1fda8e9b0ff8e881379d41321c226a01
zeus012345 <francisjerry360@gmail.com>
2023-01-04 23:47:24 +0100

    added loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import { useState } from "react";
-import { BrowserRouter, Routes, Route, useNavigate, Navigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useNavigate,
+  Navigate,
+} from "react-router-dom";
 import Login from "./pages/login";
 import Dashboard from "./pages/dashboard";
 import Users from "./pages/users";
 import DashboardUsers from "./pages/dashboardUser";
 import SharedLayout from "./components/SharedLayout/SharedLayout'/SharedLayout";
 import Guarantor from "./pages/guarantor";
+import loader from "./assets/logo.svg";
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   // save login session to local storage
   const login = () => {
@@ -22,6 +30,22 @@ function App() {
     setIsAuthenticated(false);
   };
 
+  // how to add preloader
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
+  //  add preloader
+  if (loading) {
+    return (
+      <div className="preloader ">
+        <img className="heartbeat" src={loader} alt="loader" />
+      </div>
+    );
+  }
+
   return (
     <BrowserRouter>
       <Routes>
@@ -32,8 +56,7 @@ function App() {
           <Route path="/users" element={<Users />} />
           <Route path="/dashboard/:userId" element={<DashboardUsers />} />
         </Route>
-        <Route path="*" element={<Navigate to="/"/>}/>
-       
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
   );
